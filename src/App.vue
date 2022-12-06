67cc0368ceec5d4f981fea2f5c11177b3631cfb0
shlee9605 <shlee9605@naver.com>
2022-12-06 15:56:53 +0900

    ver0.1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <edukit />
+  </div>
+</template>
+
+<script>
+import edukit from './views/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    edukit
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <helloEdukit />
+</template>
+
+<script>
+import Three from "../js/example";
+
+let helloEdukit = {
+    template: '<div ref="webgl"></div>',
+    mounted(){
+        Three(this.$refs.webgl);
+    }
+}
+
+export default {
+    name: "edukit",
+    components: {
+        helloEdukit
+    }
+}
+</script>
+
+<style scoped>
+</style>
